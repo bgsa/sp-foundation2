908cec1135027668b915b519e23882f8dd807f7c
bgsa <brunogsa@live.com>
2022-06-24 10:23:52 -0300

    -

--- build/build-linux.sh
+++ build/build-linux.sh
@@ -7,12 +7,12 @@ source build-base.sh
 make_build_dir
 cd $BUILD_DIR
 
-cmake ..\..\ -G "Unix Makefiles"               ^
-	-DOPERATING_SYSTEM:STRING=LINUX            ^
-	-DARCH:STRING=x86_64                       ^
-	-DBUILD_SHARED_LIBS:BOOL=ON                ^
-	-DCMAKE_BUILD_TYPE:STRING=Debug            ^
-	-DFFMPEG_ENABLED:BOOL=ON                   ^
+cmake ../../ -G "Unix Makefiles"               \
+	-DOPERATING_SYSTEM:STRING=LINUX            \
+	-DARCH:STRING=x86_64                       \
+	-DBUILD_SHARED_LIBS:BOOL=ON                \
+	-DCMAKE_BUILD_TYPE:STRING=Debug            \
+	-DFFMPEG_ENABLED:BOOL=ON                   \
 	-DCMAKE_ENABLE_EXPORTS:BOOL=ON
 
 cmake --build . --config Debug
